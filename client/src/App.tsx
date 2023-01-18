1c7fcdf581633a6e38f6a87eb45b9b68e799a74c
Chingiz Shadiev <ch.shadiev@gmail.com>
2023-01-18 22:19:45 +0300

    basic project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+
+import "./App.sass";
+
+export const App = () => {
+  return (
+    <div className={"app-container"}>
+      <Routes>{/* <Route path="/" element={""} /> */}</Routes>
+    </div>
+  );
+};
