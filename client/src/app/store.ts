f6866123a3519cd2827dad00af39100df1cc011b
cshadiev <ch.shadiev@gmail.com>
2023-04-29 11:04:49 +0000

    remove sample counter feature

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,29 +1,27 @@
 import {
-  configureStore,
-  ThunkAction,
-  Action,
+    configureStore,
+    ThunkAction,
+    Action,
 } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import { taskPanelReducer } from "../features/taskPanel/taskPanelSlice";
 import { countDownReducer } from "../features/countDownTimer/countDownSlice";
 import { sessionJournalReducer } from "../features/sessionJournal/sessionJournalSlice";
 import { taskInfoReducer } from "../features/taskInfo/taskInfoSlice";
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    taskPanel: taskPanelReducer,
-    countDown: countDownReducer,
-    sessionJournal: sessionJournalReducer,
-    taskInfo: taskInfoReducer,
-  },
+    reducer: {
+        taskPanel: taskPanelReducer,
+        countDown: countDownReducer,
+        sessionJournal: sessionJournalReducer,
+        taskInfo: taskInfoReducer,
+    },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
 >;
