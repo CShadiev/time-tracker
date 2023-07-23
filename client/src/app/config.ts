27d10e95adc4d91584d47f62baccac0ae87a4c96
cshadiev <ch.shadiev@gmail.com>
2023-07-23 20:37:11 +0300

    -

--- client/src/app/config.ts
+++ client/src/app/config.ts
@@ -1,4 +1,4 @@
-export const baseName = "/";
+export const baseName = "/time-tracker";
 export const apiBase = "https://cshadiev.com/time-tracker/api";
 // export const apiBase = "http://localhost:5000";
 export const unprotectedRoutes = ["/users/token/"];
